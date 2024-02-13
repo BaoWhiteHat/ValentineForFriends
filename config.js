8cacaf835b96087d7747bf49914deaa2e0441357
BaoBountyHunter <143303204+BaoBountyHunter@users.noreply.github.com>
2024-02-14 00:19:19 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Mình có điều này muốn hỏi bạn, Bạn nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Làm gì có ai đẹp trai bằng Bảo đúng hông ? 😙',
+    desc: 'Mày không trả lời hoặc thoát ra, tức là Mày công nhận đó nha :> ',
+    btnYes: 'Đúng rồi xưa giờ làm gì ai đẹp bằng Bảo 💖💖💖',
+    btnNo: '💔 Có cái lồn ! 💔 ',
+    question:'Nói thử coi lý do nào m nói t đẹp trai 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh Bảo vừa tinh tế, kinh tế lại tử tế đó Ahiiiii 😊😊',
+    mess: 'Giỡn thôi, Bảo chúc những cô gái được Bảo gửi link này mãi xinh đẹp, đánh nền không mốc, tán má không bết, kẻ mắt 5 giây, gắn mi nhoay nhoáy và sớm tìm được người hợp với chính mình nhá  🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Call me. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.messenger.com/t/100030964275189'
+}
